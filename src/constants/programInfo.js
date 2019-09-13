2427a98f45c5a05339b2d9568a03b02b5d08cf25
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-13 11:37:47 -0500

    Fill in template with Codesmith info

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -1,4 +1,4 @@
-import logo from "../images/logo_placeholder.png" // add school logo to images folder and import here
+import logo from "../images/logo_codesmith.png" // add school logo to images folder and import here
 
 export const applicationsLive = true // set to false if not currently accepting applications
 export const disabledLoanAppFormID = '69140c47-bb03-4a1b-b674-cc136fdea23d' // Hubspot ID for all disabled apply now lead captures
@@ -9,23 +9,23 @@ export const nextCohortStartDate = 'October 29, 2019'
 export const schoolInfo = { // term details section
     interestRate36: '8.99%',
     interestRate60: '10.99%',
-    APRRange36: 'XX.XX - XX.XX%',
-    APRRange60: 'XX.XX - XX.XX%',
+    APRRange36: '11.16%',
+    APRRange60: '12.51%',
     interestOnly: [
         { // for multiple examples of interest only, add items to interestOnly array
-            programName: 'PROGRAM NAME',
-            APR36: 'XX.XX%',
-            financeCharge36: '$X,XXX.XX',
-            IOPayment36: '$XX.XX',
-            FullMonthlyPayment36: '$XXX.XX',
-            APR60: 'XX.XX%',
-            financeCharge60: '$X,XXX.XX',
-            IOPayment60: '$XX.XX',
-            FullMonthlyPayment60: '$XXX.XX',
-            LoanExampleAmt: '$10,000',
-            LoanExampleOFeeAmt: '$400',
-            LoanExampleAmtPlusOFee: '$10,400',
-            programLength: 'X' // program length in months
+            programName: '',
+            APR36: '11.16%',
+            financeCharge36: '$2,752',
+            IOPayment36: '$93.50',
+            FullMonthlyPayment36: '$396.80',
+            APR60: '12.51%',
+            financeCharge60: '$4,848',
+            IOPayment60: '$114.30',
+            FullMonthlyPayment60: '$271.28',
+            LoanExampleAmt: '$12,000',
+            LoanExampleOFeeAmt: '$480',
+            LoanExampleAmtPlusOFee: '$12,480',
+            programLength: '3' // program length in months
         }
     ],
     immediateRepayment: [
@@ -45,41 +45,35 @@ export const schoolInfo = { // term details section
 }
 
 export const faq = { // faq section
-    costOfLiving: false, // true if at least one program has cost of living included
+    costOfLiving: true, // true if at least one program has cost of living included
     costOfLivingPrograms: '', // leave as empty string is cost of living availability is the same across all programs
-    multCostOfLivingPrograms: true, // true if costOfLivingPrograms string has more than one program
+    multCostOfLivingPrograms: false, // true if costOfLivingPrograms string has more than one program
     interestOnly: true, // true if interest-only payments are an option
-    immediateRepayment: true, // true if immediate repayment is an option
+    immediateRepayment: false, // true if immediate repayment is an option
     multipleLoanLengths: true, // true if 36 and 60 month options are both available
-    multipleLoanTypes: true, // true if both IR and IO are available
-    multiPrograms: true, // only true if there are multiple programs
+    multipleLoanTypes: false, // true if both IR and IO are available
+    multiPrograms: false, // only true if there are multiple programs
     onlinePrograms: false, // true if at least one program is remote/online
     schoolHQState: 'WA',
     origFee: 0.04,
 
     // interest payment FAQ info
-    exampleLoanAmount: '$10,000',
+    exampleLoanAmount: '$12,000',
     interestRate36: '8.99%',
     interestRate60: '10.99%',
-    APR36: 'XX.XX - XX.XX%',
-    APR60: 'XX.XX - XX.XX%',
-    IOPayment36: '$77.91',
-    IOPayment60: '$95.25',
+    APR36: '11.16%',
+    APR60: '12.51%',
+    IOPayment36: '$93.50',
+    IOPayment60: '$114.30',
 
     // max loan amounts by program for faq1
     loanRange: [
         {
-            programName: "PROGRAM 1",
-            maxAmount: "the max loan amount",
+            programName: "Software Engineering Immersive",
+            maxAmount: "$17,700",
             col: true,
             colAmount: "$6,000"
-        },
-        {
-            programName: "PROGRAM 2",
-            maxAmount: "$10,750",
-            col: false,
-            colAmount: "$6,000"
-        },
+        }
     ]
 }
 
@@ -89,43 +83,43 @@ export const faq = { // faq section
 
 export const schoolLogo = logo // go to header.js if height needs adjustment
 
-export const schoolName = "SCHOOL_NAME"
+export const schoolName = "Codesmith"
 
-export const schoolURL = 'https://www.SCHOOLWEBSITE.com/' // update with url of school's website
+export const schoolURL = 'https://www.codesmith.io/' // update with url of school's website
 
-export const skfURL = 'https://SCHOOLNAME.skills.fund' // update with Skills Fund url
+export const skfURL = 'https://codesmith.skills.fund' // update with Skills Fund url
 
-export const headline = "Learn to Code at SCHOOL NAME" // update headline as appropriate
+export const headline = "Learn to Code at Codesmith" // update headline as appropriate
 
 export const leadContent = {
     header: "Your last step on the path toward changing your career",
-    paragraph: `${schoolName} designs its programs to cover multiple stacks of programming languages to help graduates develop a diverse skill-set. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving ? ' and cost of living' : ''} financing so more students like you can access their program.`
+    paragraph: `${schoolName}'s Software Engineering Immsersive program teaches full-stack JavaScript to prepare you for a career in development. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving ? ' and cost of living' : ''} financing so more students like you can access their program.`
 }
 
 export const threeStepCardText = {
     step1: "",
     step2: {
         header: "select your program",
-        text: "Choose between the Onsite Bootcamp, Online Full-Time Bootcamp, and Online Part-Time Bootcamp programs."
+        text: "Codesmith focuses exclusively on its Software Engineering Immersive program."
     },
     step3: `You'll be on your way to an exciting career in tech as part of ${schoolName}'s powerful network.`
 }
 
-export const netlifyFormName = 'SCHOOLNAME_contact'
+export const netlifyFormName = 'codesmith_contact'
 
 export const GATracking = 'UA-68312423-1'
 
-export const hubspotFormId = "HUBSPOT_FORM_ID" // create Hubspot form, get form id after publishing
+export const hubspotFormId = "999b0e7d-8bd3-4d81-9989-edd047ff74d1" // create Hubspot form, get form id after publishing
 
-export const selectAProgram = "select_a_SCHOOLNAME_program" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
+export const selectAProgram = "program_name" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
 
 // ***** END GENERAL SCHOOL INFO ***** 
 
 
 // ***** BEGIN LOAN APP AND CALC INFO *****
 
-export const defaultLoanAmount = 10000
-export const placeholder = '$10,000'
+export const defaultLoanAmount = 12000
+export const placeholder = '$12,000'
 export const interestRates = {
     ir36: 8.99,
     ir60: 10.99
@@ -134,10 +128,10 @@ export const moreThanSixPrograms = false // set to true if there are 7 or more p
 
 export const programLoanInfo = [ // update with program names and corresponding loan URLs with market segment code from Master Loan Parameters
     {
-        name: "Program 1",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE1",
+        name: "Software Engineering Immersive",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKCSMItH19",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 19495,
+            maxLoanAmt: 23700,
             loanTerm36: true,
             loanTerm60: true,
             '0': { // interest-only 
@@ -155,7 +149,7 @@ export const programLoanInfo = [ // update with program names and corresponding 
           {
             location: "Metro 1",
             loanInfo: { // // match loanInfo to Program 1 above
-              maxLoanAmt: 19495,
+              maxLoanAmt: 23700,
               loanTerm36: true,
               loanTerm60: true,
               '0': { 
@@ -323,7 +317,7 @@ export const programLoanInfo = [ // update with program names and corresponding 
 ]
 
 // ***** BEGIN LOAN CALC TEXT INFO *****
-export const programMaxText = "Choose the loan amount that works best for you. Borrow up to your metro's max (see table below) for the PROGRAM 1, up to $14,995 for the PROGRAM 2 Bootcamp tuition, and up to $9,995 for the PROGRAM 3 Bootcamp tuition."
+export const programMaxText = "Choose the loan amount that works best for you. Borrow up to $17,700 for the Software Engineering Immersive tuition & $6,000 for cost of living"
 
 export const paymentTable = {
     headers: ["Program", "Tuition", "Cost of Living", "Max Total"],
